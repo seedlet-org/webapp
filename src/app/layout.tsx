159e82e04c582565bf3a90115609990d0a2718fa
goldenhub <goldenazubuike@gmail.com>
2025-06-08 22:20:28 +0100

    chore: folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import {Providers} from "../utils/providers";
+import { Providers } from "../lib/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,12 +25,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Providers>
-          {children}
-        </Providers>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
